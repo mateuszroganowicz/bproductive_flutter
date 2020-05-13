4ff743a6f1f4a0b5216bf598bf53a66f63a0d33a
Mateusz <matiroganowicz@gmail.com>
2020-05-13 02:01:46 +0200

    bottom navigation bar created with 5 pages

---
+++ lib/Mood.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MoodPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+
+    return Container(
+      child: Text("MoodPage"),
+    );
+  }
+}
---
+++ lib/Music.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MusicPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+
+    return Container(
+      child: Text("Music"),
+    );
+  }
+}
