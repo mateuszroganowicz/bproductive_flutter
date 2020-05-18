a6a75e660f1dd6d4384af278955407629554bd76
Mateusz <matiroganowicz@gmail.com>
2020-05-19 00:20:08 +0200

    mood layout created

--- lib/Mood.dart
+++ lib/Mood.dart
@@ -4,9 +4,97 @@ class MoodPage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-
-    return Container(
-      child: Text("MoodPage"),
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.only(top: 50),
+                child: Center(
+                  child: Text(
+                    "What is your mood today?",
+                    style: TextStyle(
+                      fontSize: 30,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.only(top: 50),
+                width: 100.0,
+                height: 100.0,
+                child: FlatButton(
+                  padding: EdgeInsets.all(0.0),
+                  onPressed: (){},
+                  child: Image.asset('assets/001-smile.png'),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 50),
+                width: 100.0,
+                height: 100.0,
+                child: FlatButton(
+                  padding: EdgeInsets.all(0.0),
+                  onPressed: (){},
+                  child: Image.asset('assets/002-sad.png'),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 50),
+                width: 100.0,
+                height: 100.0,
+                child: FlatButton(
+                  padding: EdgeInsets.all(0.0),
+                  onPressed: (){},
+                  child: Image.asset('assets/012-stress.png'),
+                ),
+              )
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.only(top: 50),
+                width: 100.0,
+                height: 100.0,
+                child: FlatButton(
+                  padding: EdgeInsets.all(0.0),
+                  onPressed: (){},
+                  child: Image.asset('assets/015-shock.png'),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 50),
+                width: 100.0,
+                height: 100.0,
+                child: FlatButton(
+                  padding: EdgeInsets.all(0.0),
+                  onPressed: (){},
+                  child: Image.asset('assets/038-angry.png'),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 50),
+                width: 100.0,
+                height: 100.0,
+                child: FlatButton(
+                  padding: EdgeInsets.all(0.0),
+                  onPressed: (){},
+                  child: Image.asset('assets/042-cool.png'),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
     );
   }
 }
