0232f01c7f2bf5ed69eee6b1fd097b11f210f0f5
mateuszroganowicz <matiroganowicz@gmail.com>
2020-05-28 02:47:12 +0200

    music page working, small changes tbd (progress bar and style)

--- lib/ViewModels/Music.dart
+++ lib/ViewModels/Music.dart
@@ -1,81 +1,161 @@
+import 'package:flutter_audio_query/flutter_audio_query.dart';
+import 'package:assets_audio_player/assets_audio_player.dart';
 import 'package:flutter/material.dart';
 
 import '../main.dart';
 
-class MusicPage extends StatelessWidget{
+class MusicPage extends StatefulWidget {
+  @override
+  _MusicPageState createState() {
+
+    return _MusicPageState();
+  }
+}
+
+class _MusicPageState extends State<MusicPage> {
+  final FlutterAudioQuery audioQuery = FlutterAudioQuery();
+  final assetsAudioPlayer = AssetsAudioPlayer();
+  int position = 0;
+  bool playing = false;
+  List<SongInfo> _songs;
+  final audios = <Audio>[];
+
+  @override
+  void initState() {
+    super.initState();
+    getSongs();
+  }
+
+  void getSongs() async{
+    var songs = await audioQuery.getSongs();
+    songs = new List.from(songs);
+    setState(() {
+      _songs = songs;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
+    return Scaffold(backgroundColor: Colors.white,
 
-    return Scaffold(
-      body: Container(
-          margin: EdgeInsets.fromLTRB(5, 20, 5, 20),
-          decoration: BoxDecoration(borderRadius: BorderRadius.circular(12.0), color: MyApp.secondary_color),
-          child: ListView(
-            children: <Widget>[
-              Container(
-                height: 50,
-                decoration: BoxDecoration(borderRadius: BorderRadius.circular(12.0), color: MyApp.secondary_color_3,),
-                child: const Center(
-                    child: Text(
-                      'Song1',
-                      style: TextStyle
-                        (fontWeight: FontWeight.bold, color: Colors.white),
-                    )
+      body: Stack(
+        children: <Widget>[
+          new ListView.builder(
+              itemCount: _songs.length,
+              itemBuilder: (context, int index){
+                return new ListTile(
+                  leading: CircleAvatar(
+                    child: new Text(_songs[index].title[0]),
+                  ),
+                  title: new Text(_songs[index].title),
+                  onTap: () async{
+                    position = index;
+                    String filePath = _songs[index].filePath;
+
+                    await assetsAudioPlayer.open(Audio.file(filePath));
+                     assetsAudioPlayer.play();
+                     setState(() {
+                       playing = true;
+                     });
+
+                  },
+                );
+              }
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width*0.8,
+            height: 80,
+            margin: EdgeInsets.only(left: MediaQuery.of(context).size.width*0.1, top: MediaQuery.of(context).size.height*0.6),
+            decoration: BoxDecoration(color: Colors.white),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+
+              children: <Widget>[
+                IconButton(
+                icon: Icon(Icons.skip_previous),
+                onPressed: () async {
+                  if (position > 0) {
+                    position--;
+                    String filePath = _songs[position].filePath;
+                    await assetsAudioPlayer.open(Audio.file(filePath));
+                    assetsAudioPlayer.play();
+                    playing = true;
+                    print(position);
+                  }
+                  else{
+                    position = _songs.length - 1;
+                    String filePath = _songs[position].filePath;
+                    await assetsAudioPlayer.open(Audio.file(filePath));
+                    assetsAudioPlayer.play();
+                    playing = true;
+                    print(position);
+
+                  }
+                },
                 ),
-              ),
-              Container(
-                height: 50,
-                decoration: BoxDecoration(borderRadius: BorderRadius.circular(12.0), color: MyApp.secondary_color_2,),
-                child: const Center(
-                    child: Text(
-                      'Song2',
-                      style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
-                    )
+
+                IconButton(
+                    icon: Icon(playing ? Icons.pause : Icons.play_arrow),
+                    onPressed: (){
+                        assetsAudioPlayer.playOrPause();
+                        if(playing){
+                          setState(() {
+                            playing = false;
+                          });
+                        }
+                        else{
+                          setState(() {
+                            playing = true;
+                          });
+                        }
+                    }
                 ),
-              ),
-              Container(
-                height: 50,
-                decoration: BoxDecoration(borderRadius: BorderRadius.circular(12.0), color: MyApp.secondary_color_3,),
-                child: const Center(
-                    child: Text(
-                      'Song3',
-                      style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
-                    )
+                SizedBox (
+                    width: 16
                 ),
-              ),
-              Container(
-                height: 50,
-                decoration: BoxDecoration(borderRadius: BorderRadius.circular(12.0), color: MyApp.secondary_color_2,),
-                child: const Center(
-                    child: Text(
-                      'Song4',
-                      style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
-                    )
+                IconButton(
+                    icon: Icon(Icons.stop),
+                    onPressed: (){
+                      assetsAudioPlayer.stop();
+                      setState(() {
+                        playing = false;
+                      });
+                    }
                 ),
-              ),
-              Container(
-                height: 50,
-                decoration: BoxDecoration(borderRadius: BorderRadius.circular(12.0), color: MyApp.secondary_color_3,),
-                child: const Center(
-                    child: Text(
-                      'Song5',
-                      style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
-                    )
-                ),
-              ),
-            ],
+                IconButton(
+                  icon: Icon(Icons.skip_next),
+                  onPressed: () async {
+                    if(position < _songs.length-1){
+                        position++;
+                        String filePath = _songs[position].filePath;
+                        await assetsAudioPlayer.open(Audio.file(filePath));
+                        assetsAudioPlayer.play();
+                        playing = true;
+                        print(position);
+
+                    }
+                    else{
+                      position = 0;
+                      String filePath = _songs[position].filePath;
+                      await assetsAudioPlayer.open(Audio.file(filePath));
+                      assetsAudioPlayer.play();
+                      playing = true;
+                      print(position);
+
+                    }
+                  },
+                )
+
+              ],
+            ),
+
           )
+        ],
+
       ),
 
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(
-          Icons.play_arrow,
-          color: MyApp.secondary_color,
-        ),
-        backgroundColor: MyApp.main_color,
-      ),
     );
   }
 }
