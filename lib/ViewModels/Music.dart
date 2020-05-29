5368cf9b650529b27c8c265a66d030263ee247c6
mateuszroganowicz <matiroganowicz@gmail.com>
2020-05-29 02:03:29 +0200

    added slider, curretly playing and new buttons

--- lib/ViewModels/Music.dart
+++ lib/ViewModels/Music.dart
@@ -15,33 +15,46 @@ class MusicPage extends StatefulWidget {
 class _MusicPageState extends State<MusicPage> {
   final FlutterAudioQuery audioQuery = FlutterAudioQuery();
   final assetsAudioPlayer = AssetsAudioPlayer();
+  Duration currentTime = Duration();
+  Duration totalTime = Duration();
   int position = 0;
   bool playing = false;
-  List<SongInfo> _songs;
-  final audios = <Audio>[];
-
+  List<SongInfo> _songs = [];
+  String title = "";
   @override
   void initState() {
     super.initState();
+    assetsAudioPlayer.stop();
     getSongs();
+
+  }
+
+  @override
+  void dispose() {
+    assetsAudioPlayer.dispose();
+    super.dispose();
   }
 
   void getSongs() async{
     var songs = await audioQuery.getSongs();
     songs = new List.from(songs);
-    setState(() {
-      _songs = songs;
-    });
+
+      setState(() {
+        _songs = songs;
+      });
+
+
+
   }
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(backgroundColor: Colors.white,
-
       body: Stack(
         children: <Widget>[
-          new ListView.builder(
+          Container(
+            height: 300,
+            child: _songs != [] ? new ListView.builder(
               itemCount: _songs.length,
               itemBuilder: (context, int index){
                 return new ListTile(
@@ -50,11 +63,24 @@ class _MusicPageState extends State<MusicPage> {
                   ),
                   title: new Text(_songs[index].title),
                   onTap: () async{
+                    assetsAudioPlayer.stop();
                     position = index;
                     String filePath = _songs[index].filePath;
-
+                    title = _songs[index].title;
                     await assetsAudioPlayer.open(Audio.file(filePath));
                      assetsAudioPlayer.play();
+                    assetsAudioPlayer.currentPosition.listen((Duration duration) {
+                      setState(() {
+                        currentTime = duration;
+                      });
+                    });
+
+                    assetsAudioPlayer.current.listen((playingAudio) {
+                      setState(() {
+                        totalTime = playingAudio.audio.duration;
+                      });
+                    });
+
                      setState(() {
                        playing = true;
                      });
@@ -62,10 +88,20 @@ class _MusicPageState extends State<MusicPage> {
                   },
                 );
               }
+          ):Text("No songs found"),
           ),
+
           Container(
             width: MediaQuery.of(context).size.width*0.8,
-            height: 80,
+            height: 60,
+            margin: EdgeInsets.only(left: MediaQuery.of(context).size.width*0.1, top: MediaQuery.of(context).size.height*0.5),
+            decoration: BoxDecoration(color: Colors.white),
+            child: Text("Currently playing: " + title),
+          ),
+
+          Container(
+            width: MediaQuery.of(context).size.width*0.8,
+            height: 60,
             margin: EdgeInsets.only(left: MediaQuery.of(context).size.width*0.1, top: MediaQuery.of(context).size.height*0.6),
             decoration: BoxDecoration(color: Colors.white),
             child: Row(
@@ -73,89 +109,156 @@ class _MusicPageState extends State<MusicPage> {
               mainAxisSize: MainAxisSize.max,
 
               children: <Widget>[
-                IconButton(
-                icon: Icon(Icons.skip_previous),
-                onPressed: () async {
-                  if (position > 0) {
-                    position--;
-                    String filePath = _songs[position].filePath;
-                    await assetsAudioPlayer.open(Audio.file(filePath));
-                    assetsAudioPlayer.play();
-                    playing = true;
-                    print(position);
-                  }
-                  else{
-                    position = _songs.length - 1;
-                    String filePath = _songs[position].filePath;
-                    await assetsAudioPlayer.open(Audio.file(filePath));
-                    assetsAudioPlayer.play();
-                    playing = true;
-                    print(position);
+                Container(
+                  height: 50,
+                  width: 50,
+                  child: FlatButton(
+                    onPressed: () async {
+                      if (position > 0) {
+                        position--;
+                        String filePath = _songs[position].filePath;
+                        title = _songs[position].title;
+                        await assetsAudioPlayer.open(Audio.file(filePath));
+                        assetsAudioPlayer.play();
+                        playing = true;
 
-                  }
-                },
+                        print(position);
+                      }
+                      else{
+                        position = _songs.length - 1;
+                        String filePath = _songs[position].filePath;
+                        title = _songs[position].title;
+                        await assetsAudioPlayer.open(Audio.file(filePath));
+                        assetsAudioPlayer.play();
+                        playing = true;
+
+                        print(position);
+
+                      }
+                    },
+                    child: Image.asset('assets/previous.png'),
+                  ),
                 ),
 
-                IconButton(
-                    icon: Icon(playing ? Icons.pause : Icons.play_arrow),
-                    onPressed: (){
+
+
+                Container(
+                  height: 70,
+                  width: 70,
+                  child: FlatButton(
+                      child: Image.asset(playing ? 'assets/pause.png' : 'assets/play.png'),
+                      onPressed: () async{
                         assetsAudioPlayer.playOrPause();
                         if(playing){
                           setState(() {
+                            title = _songs[position].title;
                             playing = false;
+
                           });
                         }
                         else{
                           setState(() {
+                            title = _songs[position].title;
                             playing = true;
+
                           });
                         }
-                    }
+                      }
+                  ),
+
                 ),
-                SizedBox (
-                    width: 16
+
+                Container(
+                  height: 70,
+                  width: 70,
+                  child: FlatButton(
+                      child: Image.asset('assets/stop.png'),
+                      onPressed: (){
+                        assetsAudioPlayer.stop();
+                        setState(() {
+                          playing = false;
+                          title = "";
+                        });
+                      }
+                  ),
+
                 ),
-                IconButton(
-                    icon: Icon(Icons.stop),
-                    onPressed: (){
-                      assetsAudioPlayer.stop();
-                      setState(() {
-                        playing = false;
-                      });
-                    }
-                ),
-                IconButton(
-                  icon: Icon(Icons.skip_next),
-                  onPressed: () async {
-                    if(position < _songs.length-1){
+                Container(
+                  height: 50,
+                  width: 50,
+                  child: FlatButton(
+                    child: Image.asset('assets/next.png'),
+                    onPressed: () async {
+                      if(position < _songs.length-1){
                         position++;
                         String filePath = _songs[position].filePath;
+                        title = _songs[position].title;
+                        await assetsAudioPlayer.open(Audio.file(filePath));
+                        assetsAudioPlayer.play();
+                        playing = true;
+                        print(position);
+                      }
+                      else{
+                        position = 0;
+                        String filePath = _songs[position].filePath;
+                        title = _songs[position].title;
                         await assetsAudioPlayer.open(Audio.file(filePath));
                         assetsAudioPlayer.play();
                         playing = true;
                         print(position);
 
-                    }
-                    else{
-                      position = 0;
-                      String filePath = _songs[position].filePath;
-                      await assetsAudioPlayer.open(Audio.file(filePath));
-                      assetsAudioPlayer.play();
-                      playing = true;
-                      print(position);
+                      }
+                    },
+                  ),
 
-                    }
-                  },
                 )
 
               ],
             ),
 
-          )
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width*0.8,
+            height: 40,
+            margin: EdgeInsets.only(left: MediaQuery.of(context).size.width*0.1, top: MediaQuery.of(context).size.height*0.7),
+            decoration: BoxDecoration(color: Colors.white),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              children: <Widget>[
+                Text(currentTime.toString().split(".")[0]),
+                Slider(
+                  value: currentTime.inSeconds.toDouble(),
+                  min: 0.0,
+                  max: totalTime.inSeconds.toDouble(),
+                  onChanged: (double value){
+                    setState(() {
+                      seekToSec(value.toInt());
+                      value = value;
+                    });
+                  },
+                ),
+
+                Text((totalTime-currentTime).toString().split(".")[0]),
+              ],
+
+            ),
+
+          ),
+
         ],
 
       ),
 
     );
+  }
+
+
+
+
+  void seekToSec(int second){
+    Duration newDuration = Duration(seconds: second);
+    assetsAudioPlayer.seek(newDuration);
+
   }
 }
